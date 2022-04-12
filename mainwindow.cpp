02d975921def4637a721177b30d9fb460a12fe00
BoXunTong <2636175T@student.gla.ac.uk>
2022-04-12 22:28:24 +0100

    mainwindow_v2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,54 +1,126 @@
-#include "mainwindow.h"
+ #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include<opencv2/opencv.hpp>
 
-using namespace cv;
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
 {
-    printf("init...");
     ui->setupUi(this);
-    intervalTimer = new QTimer(this);
-    pixmap = new QPixmap();
-    scene = new QGraphicsScene();
-    connect(ui->openCamera, SIGNAL(clicked()), this, SLOT(start_clicked()));
-    connect(ui->closeCamera, SIGNAL(clicked()), this, SLOT(stop_clicked()));
+
+    // start detection threads
+    mWeight_detect_thread  = new Weight_detect_thread();
+    // sliding bar for measuring feeding weights
+    mWeight_detect_thread->setWeightValue = ui->horizontalSlider;
+
+    // threads for pet detection
+    mDetect_pet_thread = new Detect_pet_thread();
+    initPin();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-    delete pixmap;
-    delete scene;
-    delete intervalTimer;
+}
+// click on the test button to start the testing process which includes
+// 1. Starting the weight sensor h711 detection thread
+// 2. Starting the imaging pet detection threads
+void MainWindow::on_start_detect_clicked()
+{
+    detect_w = true;
+    ui->horizontalSlider->setMinimum(0);
+    ui->horizontalSlider->setMaximum(100);
+    ui->horizontalSlider->setSingleStep(1);
+
+    mDetect_pet_thread->start();
+    mWeight_detect_thread->start();
 }
 
-void MainWindow::start_clicked() {
-    printf("connect on_staet_slot \n");
-    bool ret = capture.open("traffic_light.mp4");
-    if(!ret) {
-        printf("open video or camera failed!\n");
-        return;
+// Turning on the camera preview
+void MainWindow::on_open_camera_clicked()
+{
+    Time_camera = new QTimer(this);
+    connect(Time_camera, SIGNAL(timeout()), this, SLOT(open_camera_time_click()));
+    Time_camera->start(30);//30ms refreshing camera fps=30
+    frame = cap.open(0);
+    //frame = cap.open("1.mp4");
+    if(!cap.isOpened()){
+        printf("open camera failed !");
     }
-    connect(intervalTimer, SIGNAL(timeout()), this, SLOT(capturing()));
-    printf("connect intervalTimer \n");
-    intervalTimer->start(33);
+
 }
 
-void MainWindow::capturing() {
-    printf("connect capturing... \n");
-    capture.read(frame);
-    QImage result = mMatToQimage.QImageFromMat(frame);
-    pixmap = new QPixmap(QPixmap::fromImage(result));
-    scene = new QGraphicsScene(ui->graphicsView);
-    scene->addPixmap(*pixmap);
-    scene->setSceneRect(pixmap->rect());
-    ui->graphicsView->setScene(scene);
+
+// Camera refreshing
+// Imaging refreshing
+// Obtaining weight sensor values
+// Seting the weight values
+// Copying image data for pet detection
+void MainWindow::open_camera_time_click(){
+    // Executing the function every 30ms
+    float h771OutValue =0;
+    if (detect_w == true) {
+
+        h771OutValue = mWeight_detect_thread->getH711Value();
+        qDebug("run.... %f", h771OutValue);
+    }
+
+
+    // Display of set feeding weight
+    QString weightshow;
+    int setWight = ui->horizontalSlider->value();
+    weightshow.sprintf("%d (g)", setWight);// int to string
+    ui->label_set_weight_show->setText(weightshow);
+
+    if(setWight > h771OutValue)
+    {
+        //open motion
+        qDebug("water.........................");
+        digitalWrite(water_motion_pin, 1);
+        //delay(2000);
+        //digitalWrite(water_motion_pin, 0);
+        //delay(2000);
+
+    }else {
+
+    }
+
+
+    // read image
+    cap.read(frame);
+    //resize(frame, frame, Size());
+    if (mDetect_pet_thread != nullptr) {
+        mDetect_pet_thread->setMainThreadImage(frame);
+        int feedPetCount = mDetect_pet_thread->getFeedCount();
+        QString feedCountshow;
+        feedCountshow.sprintf("%d ", feedPetCount);
+        ui->feed_count->setText(feedCountshow);
+    }
+
+
+
+    // Creating display containers
+    //cvtColor(frame, frame, );
+    cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB);
+    QImage showImage = QImage(frame.data, frame.cols, frame.rows, QImage::Format_RGB888);
+    // Adding a file to the container with fileName(QString type)as its path
+    scene->addPixmap(QPixmap::fromImage(showImage));
+    // Display the contents of the container with the help of the graphicsView (QGraphicsView class) control
+    ui->graphicsView_camera->setScene(scene);
+    // Starting display
+    ui->graphicsView_camera->show();
 }
 
-void MainWindow::stop_clicked() {
-    intervalTimer->stop();
-    disconnect(intervalTimer, SIGNAL(timeout()), this, SLOT(capturing()));
-    capture.release();
+void MainWindow::initPin()
+{
+    wiringPiSetup();
+    pinMode(water_motion_pin, OUTPUT);
+
 }
+
+// exit
+void MainWindow::on_bt_stop_clicked()
+{   printf("exit....");
+    this->close();
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,38 +2,53 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QGraphicsView>
 #include <QTimer>
+#include <QGraphicsScene>
+#include <weight_detect_thread.h>
 #include <opencv2/opencv.hpp>
-#include "mattoqimage.h"
+#include "detect_pet_thread.h"
+#include <wiringPi.h>
 
-using namespace cv;
-
-namespace Ui {
-class MainWindow;
-}
+//using namespace cv;
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    // Declaring variables
+    QString setWeightValueshow;
+    Weight_detect_thread *mWeight_detect_thread;
+
+    QTimer *Time_camera ;// camera update time
+
+    //camera
+    cv::VideoCapture cap;
+    cv::Mat frame;
+    QGraphicsScene *scene = new QGraphicsScene;
+
+    // Switch detection
+    bool detect_w = false;
+
+    // Threads detection
+    Detect_pet_thread *mDetect_pet_thread;
+    void initPin();
+    int water_motion_pin = 1;
 
 private slots:
-    void start_clicked();
-    void capturing();
-    void stop_clicked();
+    // Inline
+    void on_start_detect_clicked();
+    void on_open_camera_clicked();
+    void on_bt_stop_clicked();
+
+    void open_camera_time_click();
 
 private:
     Ui::MainWindow *ui;
-    VideoCapture    capture;
-    Mat             frame;
-    QPixmap        *pixmap;
-    QTimer         *intervalTimer;
-    QGraphicsScene *scene;
-    MatToQimage mMatToQimage;
 };
-
 #endif // MAINWINDOW_H
