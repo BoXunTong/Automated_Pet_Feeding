93a8411cfa72bbbd82527320d694effc8b73d830
BoXunTong <98624810+BoXunTong@users.noreply.github.com>
2022-07-08 14:02:58 +0100

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,10 @@ MainWindow::MainWindow(QWidget *parent)
 {
 
     ui->setupUi(this);
+    initUi();
+    //检测线程
+    mDetect_pet_thread = new Detect_pet_thread();
+    mWeight_detect_thread  = new Weight_detect_thread();
 }
 
 MainWindow::~MainWindow()
@@ -20,20 +24,51 @@ void MainWindow::on_open_camera_clicked()
     connect(Time_camera, SIGNAL(timeout()), this, SLOT(open_camera_preview_time()));
     Time_camera->start(30);//30ms刷新一次camera fps=30
     mCamera_precess.openCamera();
+
 }
 
 void MainWindow::open_camera_preview_time()
 {
     Mat frame = mCamera_precess.previewCamera();
-    QImage showImage = QImage(frame.data, frame.cols, frame.rows, QImage::Format_RGB888);
-    scene->clear();
-    scene->addPixmap(QPixmap::fromImage(showImage));
-    ui->graphicsView_camera->setScene(scene);
-    ui->graphicsView_camera->show();
+    mDetect_pet_thread->setMainThreadImage(frame);
+    int h771OutValue = mWeight_detect_thread->getH711Value();
+    updateUi(frame, h771OutValue);
 }
 
 void MainWindow::on_bt_stop_clicked()
 {
     printf("exit....");
     this->close();
+}
+
+void MainWindow::on_start_detect_clicked()
+{
+    mDetect_pet_thread->start();
+    //mWeight_detect_thread->start();
+}
+
+void MainWindow::initUi()
+{
+    ui->horizontalSlider->setMinimum(0);
+    ui->horizontalSlider->setMaximum(2000);
+    ui->horizontalSlider->setSingleStep(20);
+
+}
+
+void MainWindow::updateUi(Mat mFrame, int mH771OutValue)
+{
+    QImage showImage = QImage(mFrame.data, mFrame.cols, mFrame.rows, QImage::Format_RGB888);
+    scene->clear();
+    scene->addPixmap(QPixmap::fromImage(showImage));
+    ui->graphicsView_camera->setScene(scene);
+    ui->graphicsView_camera->show();
+
+    //显示设置投喂重量
+    QString weightshow;
+    int setWight = ui->horizontalSlider->value();
+    weightshow.sprintf("%d (g)", setWight);// float to string
+    ui->label_set_weight_show->setText(weightshow);
+    QString leftover;
+    leftover.sprintf("%d (g)", mH771OutValue);// int to string
+    ui->last_value->setText(leftover);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,8 @@
 #include <QTimer>
 #include <QGraphicsScene>
 #include "camera_precess.h"
+#include "detect_pet_thread.h"
+#include "weight_detect_thread.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -18,19 +20,23 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    //检测线程
+    Detect_pet_thread *mDetect_pet_thread;
+    Weight_detect_thread *mWeight_detect_thread;
 
 private slots:
     void on_open_camera_clicked();
     void open_camera_preview_time();
     void on_bt_stop_clicked();
-    //void on_start_detect_clicked();
+    void on_start_detect_clicked();
 
 private:
     Ui::MainWindow *ui;
     QTimer *Time_camera ;//camera的更新时间
     Camera_precess mCamera_precess;
     QGraphicsScene *scene = new QGraphicsScene;
-
+    void initUi();
+    void updateUi(Mat mFrame, int mH771OutValue);
 
 };
 #endif // MAINWINDOW_H
