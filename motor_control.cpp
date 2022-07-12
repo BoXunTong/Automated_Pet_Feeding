ff1981a32df21773bef48903d5d4658051428b5f
zyc2333333 <99829031+zyc2333333@users.noreply.github.com>
2022-07-12 01:03:07 +0100

    Add files via upload

---
+++ motor_control.cpp
@@ -0,0 +1,21 @@
+#include "motor_control.h"
+
+Motor_control::Motor_control()
+{
+
+}
+
+void Motor_control::motor_run()
+{
+
+}
+
+void Motor_control::motor_stop()
+{
+
+}
+
+void Motor_control::find_pets() {
+    cout<<"callback motor ..." <<endl;
+    printf("I'am ProgramA.FunA2() and be called..\n");
+}
